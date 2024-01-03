9815108de39d83672e1d60c27219e89f4b869e0d
shanks <60050815+shanksfigarland@users.noreply.github.com>
2024-01-03 13:49:10 -0300

    update

--- install-nxc.sh
+++ install-nxc.sh
@@ -5,6 +5,6 @@ apt install pipx git
 pipx ensurepath
 pipx install git+https://github.com/Pennyw0rth/NetExec
 pipx ensurepath
-echo -n 'export PATH=/home/kali/.local/bin:$PATH' >> ~/.zshrc  
+echo -n 'export PATH=/home/kali/.local/bin:$PATH' >> ~/.zshrc # if you use bash change to .bashrc
 sleep 0.5
 reboot
