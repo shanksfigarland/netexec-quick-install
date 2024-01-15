20abe53acf2069758fa4af7c4938b704698e7dbc
shanks <60050815+shanksfigarland@users.noreply.github.com>
2024-01-15 19:38:25 -0300

    Update install-nxc.sh

--- install-nxc.sh
+++ install-nxc.sh
@@ -1,4 +1,4 @@
-#Quick and dirty script to install the NetExec on Kali or any other distro you may like.
+# Quick and dirty script to install the NetExec on Kali or any other distro you may like.
 # File: installnxc.sh
 #!/bin/bash
 apt install pipx git
